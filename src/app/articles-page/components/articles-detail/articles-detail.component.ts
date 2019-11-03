f034e110e0d9aff58ea83f5a8826d23344dd4a56
sliker92 <sliker1992@gmail.com>
2019-11-03 13:46:29 +0300

    added html and style for detail page

--- src/app/articles-page/components/articles-detail/articles-detail.component.ts
+++ src/app/articles-page/components/articles-detail/articles-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Article } from '../../models/article.interface';
+import { PostsService } from '../../../shared/services/posts.service';
 
 @Component({
   selector: 'app-articles-detail',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesDetailComponent implements OnInit {
 
-  constructor() { }
+  post;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postsService: PostsService) {}
 
   ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => this.postsService.getPostByID(+params.get('id'), 'articles'))
+      ).subscribe((data) => {
+      this.post = data;
+    });
   }
-
 }
--- src/app/news-page/components/news-detail/news-detail.component.ts
+++ src/app/news-page/components/news-detail/news-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { News } from '../../models/news.interface';
+import { PostsService } from '../../../shared/services/posts.service';
 
 @Component({
   selector: 'app-news-detail',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsDetailComponent implements OnInit {
 
-  constructor() { }
+  post;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postsService: PostsService) {}
 
   ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => this.postsService.getPostByID(+params.get('id'), 'news'))
+      ).subscribe((data) => {
+      this.post = data;
+    });
   }
-
 }
